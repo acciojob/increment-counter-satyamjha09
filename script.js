d1e378f19d73d6d80d3ee746674b55500894c2e2
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-28 21:39:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// Select the counter paragraph and the button
+const counterParagraph = document.getElementById('counter');
+const incrementButton = document.getElementById('incrementBtn');
+
+let counterValue = 0;
+
+incrementButton.addEventListener("click" , () => {
+
+	alert(`Current value: ${counterValue}`);
+
+	counterValue++;
+
+	counterParagraph.textContent = counterValue;
+})
